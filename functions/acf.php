4e92a893f4d058e7ee135728058803659321af4d
Amy Kapernick <amy@aimhigherweb.design>
2021-08-03 14:09:21 +0800

    added acf field

--- functions/acf.php
+++ functions/acf.php
@@ -879,6 +879,63 @@
 			'description' => '',
 		));
 
+		// Blocks - Last Updated
+		acf_add_local_field_group(array(
+			'key' => 'group_6108b9d47b770',
+			'title' => 'Blocks - Last Updated',
+			'fields' => array(
+				array(
+					'key' => 'field_6108b9de93291',
+					'label' => 'Date',
+					'name' => 'date',
+					'type' => 'date_time_picker',
+					'instructions' => '',
+					'required' => 1,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'display_format' => 'j F Y g:i a',
+					'return_format' => 'U',
+					'first_day' => 1,
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param' => 'block',
+						'operator' => '==',
+						'value' => 'acf/last-updated',
+					),
+				),
+			),
+			'menu_order' => 0,
+			'position' => 'normal',
+			'style' => 'seamless',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => array(
+				0 => 'permalink',
+				1 => 'the_content',
+				2 => 'excerpt',
+				3 => 'discussion',
+				4 => 'comments',
+				5 => 'revisions',
+				6 => 'slug',
+				7 => 'author',
+				8 => 'format',
+				9 => 'page_attributes',
+				10 => 'featured_image',
+				11 => 'categories',
+				12 => 'tags',
+				13 => 'send-trackbacks',
+			),
+			'active' => true,
+			'description' => '',
+		));
+
 	endif;
     
 ?>
