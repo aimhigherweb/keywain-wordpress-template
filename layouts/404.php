2228190d3184c7a94106cfcf58e213e65083e782
Amy Kapernick <amy@aimhigherweb.design>
2021-08-31 10:12:13 +0800

    tweaked 404 page

--- layouts/404.php
+++ layouts/404.php
@@ -1,1 +1,4 @@
-<blockquote>Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</blockquote>
+
+<h1>404 - Page Not Found</h1>
+
+<p class="not_found">Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
