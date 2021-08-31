6904149249142a3598b959338585faa0cf920637
Amy Kapernick <amy@aimhigherweb.design>
2021-08-31 10:07:40 +0800

    added category page and filters, to switch between categorise

--- layouts/blog.php
+++ layouts/blog.php
@@ -3,33 +3,41 @@
 
 	$page = get_option('page_for_posts');
 	$page_data = get_page($page);
-	
-	
-
+	$blog = get_permalink( get_option( 'page_for_posts' ) );
 ?>
 
 <h1><?php echo get_the_title($page); ?></h1>
 
 <?php echo apply_filters('the_content', $page_data->post_content); ?>
 
+<ul class="cats">
+	<li>
+		<a 
+			class="current"
+			href="<?php echo $blog; ?>"
+		>
+			All Posts
+		</a>
+	</li>
+	<?php foreach(get_categories() as $cat): ?>
+		<li>
+			<a href="<?php echo get_category_link($cat->term_id); ?>">
+				<?php echo $cat->name; ?>
+			</a>
+		</li>
+	<?php endforeach; ?>
+</ul>
+
 <?php if(have_posts()) :
 
 	echo '<ul class="posts">';
 		
-		while(have_posts()) : the_post(); ?>
-
-		<li class="post">
-			<img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'block_news'); ?>" alt="">
-			<h2><?php echo get_the_title(); ?></h2>
-			<div class="content">
-				<?php echo get_the_excerpt(); ?>
-			</div>
-				<a href="<?php echo get_the_permalink(); ?>" class="block_link">
-					<span class="sr-only">Read full article <?php echo get_the_title(); ?></span>
-				</a>
-			</li>
-
-		<?php endwhile;
+		while(have_posts()) : 
+			
+			the_post();
+			get_template_part('parts/post');
+	
+		endwhile;
 
 	
 	echo '</ul>'; 
--- layouts/category.php
+++ layouts/category.php
@@ -3,6 +3,8 @@
 
 	$page = get_option('page_for_posts');
 	$page_data = get_page($page);	
+	$archive = get_the_category()[0];
+	$blog = get_permalink( get_option( 'page_for_posts' ) );
 
 ?>
 
@@ -12,15 +14,36 @@
 	); ?>
 </h1>
 
+<ul class="cats">
+	<li>
+		<a 
+			href="<?php echo $blog; ?>"
+		>
+			All Posts
+		</a>
+	</li>
+	<?php foreach(get_categories() as $cat): ?>
+		<li>
+			<a 
+				class="<?php if($cat->term_id == $archive->term_id) {echo 'current';} ?>"
+				href="<?php echo get_category_link($cat->term_id); ?>"
+			>
+				<?php echo $cat->name; ?>
+			</a>
+		</li>
+	<?php endforeach; ?>
+</ul>
+
 <?php if(have_posts()) :
 
 	echo '<ul class="posts">';
 		
-		while(have_posts()) : the_post(); ?>
+		while(have_posts()) : 
+				
+			the_post();
+			get_template_part('parts/post');
 
-			<?php get_template_part('parts/post'); ?>
-
-		<?php endwhile;
+		endwhile;
 
 	
 	echo '</ul>'; 
