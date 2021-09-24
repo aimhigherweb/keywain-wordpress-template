e8d162a34385bfb71bb32061e949445addd2ec6f
Amy Kapernick <amy@aimhigherweb.design>
2021-09-24 13:37:50 +0800

    export acf to file

--- functions/acf.php
+++ functions/acf.php
@@ -171,6 +171,61 @@
 			'description' => '',
 		));
 		
+		// Category and Tag Images
+		acf_add_local_field_group(array(
+			'key' => 'group_614d196f4bbfd',
+			'title' => 'Category and Tag Photos',
+			'fields' => array(
+				array(
+					'key' => 'field_614d1982a5bdb',
+					'label' => 'Featured Image',
+					'name' => 'image',
+					'type' => 'image',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'return_format' => 'id',
+					'preview_size' => 'thumbnail',
+					'library' => 'all',
+					'min_width' => '',
+					'min_height' => '',
+					'min_size' => '',
+					'max_width' => '',
+					'max_height' => '',
+					'max_size' => '',
+					'mime_types' => '',
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param' => 'taxonomy',
+						'operator' => '==',
+						'value' => 'category',
+					),
+				),
+				array(
+					array(
+						'param' => 'taxonomy',
+						'operator' => '==',
+						'value' => 'post_tag',
+					),
+				),
+			),
+			'menu_order' => 0,
+			'position' => 'acf_after_title',
+			'style' => 'default',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => '',
+			'active' => true,
+			'description' => '',
+		));
 
 		// Blocks - Logos
 		acf_add_local_field_group(array(
