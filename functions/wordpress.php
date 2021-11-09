446e8ca03afebcf8e99cd11fc152ae06cacb3e13
Amy Kapernick <amy@aimhigherweb.design>
2021-11-09 15:38:48 +0800

    added option for google tag manager id

--- functions/wordpress.php
+++ functions/wordpress.php
@@ -235,4 +235,23 @@
         return $svg_body;
        
     }
+
+    // Add Google Tag Manager Tag
+    function custom_ga_gtm_tags ($wp_customize){
+        $wp_customize->add_section('ga_gtm_section', array(
+            'title'          => 'Google Analytics'
+        ));
+
+        $wp_customize->add_setting('gtm_tag_id', array(
+            'default'        => ' GTM-XXXXXX',
+        ) );
+
+        $wp_customize->add_control( 'gtm_tag_id', array(
+            'type' => 'text',
+            'section' => 'ga_gtm_section',
+            'label' => __( 'Enter Google Tag Manager ID' ),
+        ) );
+
+    }
+    add_action('customize_register', 'custom_ga_gtm_tags');
 ?>
--- partials/layout.php
+++ partials/layout.php
@@ -31,6 +31,7 @@
 	}
 
 	$colour = get_theme_mod( 'main_colour' );
+	$gtm_tag = get_theme_mod('gtm_tag_id');
 
 	$class = $class . ' theme_' . $colour;
 
@@ -47,6 +48,17 @@
 		<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />
 		<base href="<?php echo get_site_url(); ?>" />
+		
+		<?php if($gtm_tag) : ?>
+			<!-- Google Tag Manager -->
+				<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer', '<?php echo $gtm_tag; ?>');</script>
+			<!-- End Google Tag Manager -->
+		<?php endif; ?>
+
 		<?php wp_head(); ?>
 
 		<title><?php echo wp_get_document_title(); ?></title>
@@ -56,6 +68,12 @@
 	</head>
 
 	<body class="<?php echo $class; ?>">
+		<?php if($gtm_tag) : ?>
+			<!-- Google Tag Manager (noscript) -->
+				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $gtm_tag; ?>"
+				height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+			<!-- End Google Tag Manager (noscript) -->
+		<?php endif; ?>
 		<?php get_template_part('partials/header'); ?>
 
 		<main 
